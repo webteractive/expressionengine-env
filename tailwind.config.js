c56c799bef904f8a92c94c715962eac224108538
Glen Bangkila <hadeinvane@gmail.com>
2020-07-26 12:30:26 +0800

    Bump laravel-mix version. Bump tailwindcss version. Move from yarn to npm.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   variants: {},
+  purge: {
+    content: [
+      './assets/**/*.html',
+      './assets/**/*.js',
+      './assets/**/*.jsx',
+      './assets/**/*.ts',
+      './assets/**/*.tsx',
+      './assets/**/*.vue',
+      './system/user/templates/**/*.html'
+    ],
+    options: {
+      defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+      whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
+    },
+  },
   plugins: [],
-}
+};
